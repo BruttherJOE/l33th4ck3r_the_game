e0d1d78f7a0f25c0a3e077183710796a541bd3a2
BruttherJOE <54128914+BruttherJOE@users.noreply.github.com>
2020-04-24 04:27:28 +0800

    added : ddossing oka, MOIC
    slowly... the game shall be complete

--- l33th4ck3r.py
+++ l33th4ck3r.py
@@ -19,7 +19,7 @@ cls = lambda: os.system('cls')
 class player:
     def __init__(self):
         self.name = ''
-        self.location = ''
+        self.location = '' #location of a player
 player = player() #this calls the class player to init player with these values
 
 #### unused TITLE SCREEN ####
@@ -163,7 +163,7 @@ def terminal_boot():
 #terminal boot sequence complete
 
 def terminal_function(): 
-    player.name = "vegeta"
+    player.name = "vegeta" #placeholder for debugging
     player.location = ("C:\ " + player.name + "\ ")
 
     answer = input(player.location + "> ")
@@ -171,10 +171,12 @@ def terminal_function():
         print("desktop\npictures")
         terminal_function()
     if answer.lower() == "cd desktop":
-        player.location += "desktop"
         desktop()
+    if answer.lower() == "cd pictures":
+        pictures()
+
         
-    while answer not in ["ls","cd desktop"]:#secondary loop
+    while answer not in ["ls","cd desktop","cd pictures"]:#secondary loop
         print(answer + " is not recognised as an internal or external command, operable program, or batch file")
         answer = input(player.location + "> ")
         if answer.lower().strip() == "ls":
@@ -182,29 +184,94 @@ def terminal_function():
             terminal_function()
         if answer.lower() == "cd desktop":
             player.location += "desktop"
-            desktop()
+            terminal_function()
+        if answer.lower() == "cd pictures":
+            player.location += "pictures"
+            pictures()        
 
 def desktop():
     #print("i am the desktop")
     #print (player.location + "> ")
+    player.location = ("C:\ " + player.name + "\desktop\ ")
     answer = input(player.location + "> ")
     if answer.lower().strip() == "ls":#primary loop
-        print("Medium Orbit Ion Cannon.exe\nDw game project.py")
-        desktop()#### below undone
+        print("moic.exe\nDw game project.py")
+        desktop()##to prevent prog from exit
     if answer.lower() == "cd ~":
-        player.location += "desktop"
-        desktop()
+        terminal_function()
+    if answer.lower() == "moic.exe":
+        moic()
         
-    while answer not in ["ls","cd desktop"]:#secondary loop
+    while answer not in ["ls","cd ~","moic.exe"]:#secondary loop
         print(answer + " is not recognised as an internal or external command, operable program, or batch file")
         answer = input(player.location + "> ")
         if answer.lower().strip() == "ls":
-            print("desktop\npictures")
+            print("moic.exe\nDw game project.py")
             desktop()
-        if answer.lower() == "cd desktop":
-            player.location += "desktop"
-            desktop()
+        if answer.lower() == "cd ~":
+            terminal_function()
+        if answer.lower() == "moic.exe":
+            moic()
+
+def pictures():
+    player.location = ("C:\ " + player.name + "\pictures\ ")
+    answer = input(player.location + "> ")
+    if answer.lower().strip() == "ls":#primary loop
+        print("naked.jpg")
+        pictures() ## this loop is to prevent the program from exiting
+    if answer.lower() == "cd ~":
+        terminal_function()
+
+    while answer not in ["ls","cd ~"]:#secondary loop
+        print(answer + " is not recognised as an internal or external command, operable program, or batch file")
+        answer = input(player.location + "> ")
+        if answer.lower().strip() == "ls":
+            print("naked.jpg")
+            pictures()
+        if answer.lower() == "cd ~":
+            terminal_function()
+
+def moic():
+    cls()
+    player.location = ("// MOIC > ")
+    loadbar = ("■■■■■■■■■■■■■■■■■■■\nMOIC Loaded. \n2020 all rights reserved.\n")
+    disclaimer = ("\nThere are inherent dangers in the use of any software, and you are solely responsible for your own actions.")
+    moichelp = ("type 'help' to access MOIC commands.\ntype 'z' to exit program.")
+    delay_print_5(loadbar)
+    print(disclaimer)
+    print (moichelp)
+    #
+    answer = input(player.location)
+    if answer.lower().strip() == "help":#primary loop
+        print("this is a help file placeholder")
     
+    if answer.lower() == "ddos oka@192.168.1.1":
+        DDOSoka()
+
+    if answer.lower().strip() == "z":#primary loop
+        desktop()
+
+    while answer not in ["help","ddos oka@192.168.1.1"]:#secondary loop
+        print(answer + " is not recognised as an internal or external command, operable program, or batch file")
+        answer = input(player.location)
+        if answer.lower().strip() == "help":#primary loop
+            print("this is a help file placeholder")
+    
+        if answer.lower() == "ddos oka@192.168.1.1":
+            DDOSoka()
+
+        if answer.lower().strip() == "z":#primary loop
+            desktop()
+
+def DDOSoka():
+    print("ahhh oka is ddossed")
+    #this is where u code the show of ddossing oka
+
+
+
+
+def ssh():
+    pass
 
 
 
@@ -215,12 +282,29 @@ def help():
     print("")
     #these will be console commands before virtual terminal is launch
 
+def notes(): #nano in to view
+    cls()
+    print("MOIC - 'Medium Orbit Ion Cannon'. The purpose of this program is to DDOS an individual.")
+    print("MOIC is a denial-of-service attack application designed to attack as many as 256 URLs at the same time.\n")
+    print("To successfully hack someone, FIRST deny service of their internet by DDOSing them,")
+    print("THEN SSH into their computer and you can then change their files amidst the chaos.")
+    #my personal hacking notes
+
 def helpcmd():
-    print("cd  -  change directory\n nano  -  open text editor")
-    
-    
+    print("where x is location/directory/filename you want to open or go to,\n")
+    print("cd x  -  change directory\n nano x  -  open text editor\n")
+
+def helpmoic():
+    #this is where you display the moic commands. it is invoked when help is typed
+    pass
 
 
+
+def delay_print_5(string): #this delays print such that dont have to keep writing same thing
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.05)
 
 
 
@@ -239,14 +323,16 @@ def helpcmd():
 
 
 ###################################################
-answer = input("would u like to warp to terminal function? (yes/no)")
+answer = input("would u like to warp to moic? (yes/no)")
 
 if answer.lower().strip() == "yes": 
     #this removes all the spaces at the end and also changes caps to lowercase
     #this also applies lower() and strip() to answer
-    #maingame() #use this to shortcut call maingame
-    game_intro()
+    
     #terminal_function()
+    #maingame() #use this to shortcut call maingame
+    game_intro() #for the full game
+    #moic()
  #   answer = input("you reach a xroad left or right").lower().strip()
   #  if answer == "left":
     #    game_intro() #calls game fn
